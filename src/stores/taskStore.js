b56fa4aada3d9858e283845afd3f32be2f249c6d
AleyMon <discereale@gmail.com>
2025-05-17 23:47:38 +0200

    Integrar tasks con home

--- src/stores/taskStore.js
+++ src/stores/taskStore.js
@@ -8,7 +8,9 @@ import {
   createTask,
   getAllTasks,
   deleteTaskById,
-
+  updateTask,
+  toggleCompleteTask,
+  toggleFavoriteTask,
 } from '@/api/supabase/tasksApi'
 import { useAuthStore } from '@/stores/authStore'
 
@@ -44,11 +46,72 @@ export const useTaskStore = defineStore('tasks', () => {
     }
   }
 
+  // Eliminar una tarea por ID
+  async function removeTask(taskId) {
+    try {
+      const deletedTasks = await deleteTaskById(taskId)
+      if (deletedTasks && deletedTasks.length) {
+        tasks.value = tasks.value.filter((task) => task.id !== taskId)
+      }
+    } catch (error) {
+      console.error('Error al eliminar tarea:', error.message)
+    }
+  }
+
+  // Función para actualizar una tarea por su ID
+  async function updateTaskById(taskId, updatedFields) {
+    try {
+      const [updatedTask] = await updateTask(taskId, updatedFields)
+      if (!updatedTask) return []
+      // aviso con array vacío
+
+      const index = tasks.value.findIndex((task) => task.id === taskId)
+      if (index !== -1) {
+        tasks.value.splice(index, 1, { ...tasks.value[index], ...updatedTask })
+      }
+      return updatedTask // devuelver tarea actualizada
+    } catch (error) {
+      console.error('Error al actualizar tarea:', error)
+      return [] // retorno array vacío en caso de error
+    }
+  }
+
+  // Función para cambiar el estado de completado de una tarea
+  async function toggleTaskCompletion(taskId, completed) {
+    try {
+      const updatedTask = await toggleCompleteTask(taskId, completed)
+      if (updatedTask) {
+        const index = tasks.value.findIndex((task) => task.id === taskId)
+        if (index !== -1) {
+          tasks.value.splice(index, 1, { ...tasks.value[index], ...updatedTask })
+        }
+      }
+    } catch (err) {
+      console.error('Error al cambiar estado de tarea:', err)
+    }
+  }
+
+  async function toggleFavorite(taskId, favorite) {
+    try {
+      const updatedTask = await toggleFavoriteTask(taskId, favorite)
+      if (updatedTask) {
+        const index = tasks.value.findIndex((task) => task.id === taskId)
+        if (index !== -1) {
+          tasks.value.splice(index, 1, { ...tasks.value[index], favorite: updatedTask.favorite })
+        }
+      }
+    } catch (err) {
+      console.error('Error al cambiar estado de favorita:', err)
+    }
+  }
 
   return {
     tasks,
     addTask,
     fetchTasks,
-
+    updateTaskById,
+    removeTask,
+    toggleTaskCompletion,
+    toggleFavorite,
   }
 })
