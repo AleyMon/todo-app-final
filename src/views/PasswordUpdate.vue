4e0ea18d78df1747b3d9a9985cee7874342898b2
AleyMon <discereale@gmail.com>
2025-05-29 23:46:27 +0200

    Ajustar diseño

---
+++ src/views/PasswordUpdate.vue
@@ -0,0 +1,47 @@
+<!-- PasswordUpdate.vue -->
+<script setup>
+import { ref } from 'vue'
+import { useAuthStore } from '@/stores/authStore'
+import { useRouter } from 'vue-router'
+import AuthFormBase from '@/components/AuthFormBase.vue'
+import { useToast } from 'vue-toastification'
+import { traducirError } from '@/stores/errorMessages'
+
+const password = ref('')
+const { updatePassword } = useAuthStore()
+const router = useRouter()
+const toast = useToast() // para el pop-up
+
+const handleUpdatePassword = async () => {
+  try {
+    await updatePassword(password.value)
+    toast.success('Contraseña actualizada con éxito')
+    password.value = ''
+    // Esperar unos segundos para que el usuario vea el toast-pop-up
+    setTimeout(() => {
+      router.push('/')
+    }, 2000)
+  } catch (err) {
+    console.log('Error completo:', err)
+
+    // intenta traducir el error según la estructura que venga, archivo errorMessages.js
+    let errorMsg = ''
+    if (err.response?.data?.message) {
+      errorMsg = traducirError({ message: err.response.data.message })
+    } else if (err.message) {
+      errorMsg = traducirError(err)
+    } else {
+      errorMsg = traducirError(err)
+    }
+
+    toast.error(errorMsg)
+
+    // Si es error de sesión, redirigir a PasswordReset tras 1 segundos
+    if (err?.name === 'AuthSessionMissingError') {
+      setTimeout(() => {
+        router.push({ name: 'PasswordReset' })
+      }, 1000)
+    }
+  }
+}
+</script>
